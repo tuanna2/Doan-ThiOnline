76ae3baccedd7670d6b0df08b685a0474262b279
tuanna <tuanna2@ows.com.vn>
2019-06-03 00:22:29 +0700

    fix host db

--- models/dbconnect.js
+++ models/dbconnect.js
@@ -1,7 +1,7 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : 'heroku_19229d7d55e8896',
+      host : 'us-cdbr-iron-east-02.cleardb.net',
       user : 'bd717293221bab',
       password : '9a96ac84',
       database : 'heroku_19229d7d55e8896',
